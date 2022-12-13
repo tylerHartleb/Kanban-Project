95964ce747ab7fa3d3f7e6613f1aab7b814176d9
Tyler Hartleb <tyler@hartleb.ca>
2022-12-13 00:36:34 -0700

    Support for adding groups
    stuffs

--- src/classes/Project.ts
+++ src/classes/Project.ts
@@ -3,17 +3,31 @@ import { Members } from "./Members";
 
 export interface IProject {
     title: string,
+    id: string,
     groups: Group[],
-    members: Members
+    owner: string
+}
+
+export interface IProjectData {
+    title: string,
+    id: string,
+    owner: string
 }
 
 export class Project {
     title: string;
+    id: string;
     
     groups: Group[] = [];
-    members: Members = new Members();
+    owner: string;
 
-    constructor(title: string) {
+    constructor(id: string, title: string, owner: string) {
+        this.id = id;
         this.title = title;
+        this.owner = owner
+    }
+
+    static fromJSON(parsedObject : IProjectData): Project {
+        return new Project(parsedObject.id, parsedObject.title, parsedObject.owner);
     }
 }
